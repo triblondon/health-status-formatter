83ff0483d4daa3eae08dfc1188355c6321b68a24
George Angel <george.angel.87@gmail.com>
2015-08-13 21:48:50 +0100

    Remove debug

--- json_request.js
+++ json_request.js
@@ -1,6 +1,5 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	function(details) {
-		console.log("Cat intercepted: " + details.requestHeaders);
 		details.requestHeaders.push({name:"Accept",value:"application/json"});
 		return {requestHeaders: details.requestHeaders};
 	},
